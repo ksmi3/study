1de2d50676a894aa934fe3087e7d14ec0d490f69
sm <ksmi3@ipartners.co.kr>
2020-05-28 17:23:29 +0900

    React styled-component 로 스타일링해보기

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,17 +1,28 @@
 // App.js
 
 import React from 'react';
-import './App.css';
-import Header from './components/header'
-import Body from './components/body'
-import Footer from './components/footer'
+import styled from 'styled-components'
+import GlobalStyle from './components/common/global-style'
+
+const BaseBox = styled.div`
+  width: 100px;
+  height: 100px;
+`
+
+const RedBox = styled(BaseBox)`
+  background-color: #ff0032;
+`
+
+const BlueBox = styled(BaseBox)`
+  background-color: #00eaff;
+`
 
 function App() {
   return (
     <div>
-      <Header />
-      <Body />
-      <Footer />
+      <GlobalStyle />
+      <RedBox />
+      <BlueBox />
     </div>
   );
 }
